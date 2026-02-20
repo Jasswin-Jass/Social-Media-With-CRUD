d89552555c5c84b46abf81fd8e02376494f5879a
Jasswin-Jass <jasswinjass@gmail.com>
2026-02-21 00:02:30 +0530

    added put and delete for crud

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from traceback import print_tb
+
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
-
-from sentry_sdk import HttpTransport
 
 app = FastAPI()
 
@@ -14,12 +13,18 @@ class Post(BaseModel):
     publish:  bool = True
     rating: Optional[int] = None
 
+
 my_posts = [{"title": "ONE", "content": "This is post one dude", "id": 1}, {"title": "TWO", "content": "Second one here", "id": 2}]
 
 def find_post(id):
     for p in my_posts:
         if p["id"] == id:
                 return p
+
+def find_index(id):
+     for i, p in enumerate(my_posts):
+          if p['id'] == id:
+               return i
 
 @app.get("/")
 def root():
@@ -29,7 +34,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(new_post: Post):
     print(new_post) # new_post here is in pydantic model(data type) which has attributes that we mentiond in the base model class
     print(new_post.model_dump()) # to convert a pydatic model to a dictionary use .model_dump() method and you can use model_dump_json() for json
@@ -50,3 +55,25 @@ def get_post(id :int, respose: Response):
         # return {"message": f"post with id {id} not found"}     You can also use this instead 
     return {"post data": post}
         
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+     # deleting a post
+     index = find_index(id)
+     
+     if index == None:
+          raise HTTPException(status_code=404, detail=f"The post of id {id} is not found")
+
+     my_posts.pop(index) 
+     return Response(status_code=status.HTTP_204_NO_CONTENT) # if you send a response of 204, you shouldn't be sending any message back
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+     index = find_index(id)
+     
+     if index == None:
+          raise HTTPException(status_code=404, detail=f"The post of id {id} is not found")
+     post_dict = post.model_dump()
+     post_dict['id'] = id
+     my_posts[index] = post_dict
+     return {'data': post_dict }
+     
