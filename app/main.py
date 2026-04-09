5bc06d6015406cf926558c1d634d083a5864d74d
Jasswin-Jass <jasswinjass@gmail.com>
2026-04-09 14:24:14 +0530

    updated

--- app/main.py
+++ app/main.py
@@ -60,12 +60,17 @@ def create_posts(post: Post):
     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
                    (post.title, post.content, post.published))
     new_post = cursor.fetchone()
+
+    conn.commit()
+    
     return {"data": new_post}
 
 
 @app.get("/posts/{id}")
 def get_post(id :int, respose: Response):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    #post = find_post(id)
+    post = cursor.fetchone()
     if not post:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id {id} was not found")
