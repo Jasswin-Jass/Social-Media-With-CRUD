28a1fdf52d2657ee2f98c3018ced5469693e4a67
Jasswin-Jass <jasswinjass@gmail.com>
2026-04-12 21:29:57 +0530

    updated

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ def create_posts(post: Post):
 def get_post(id :int, respose: Response):
     cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
     #post = find_post(id)
-    post = cursor.fetchone()
+    post =cursor.fetchone()
     if not post:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id {id} was not found")
@@ -80,23 +80,33 @@ def get_post(id :int, respose: Response):
         
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-     # deleting a post
-     index = find_index(id)
-     
-     if index == None:
-          raise HTTPException(status_code=404, detail=f"The post of id {id} is not found")
+    # deleting a post
+    #index = find_index(id)
     
-     my_posts.pop(index) 
-     return Response(status_code=status.HTTP_204_NO_CONTENT) # if you send a response of 204, you shouldn't be sending any message back
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
+    index = cursor.fetchone()
+    conn.commit()
+    
+    if index == None:
+        raise HTTPException(status_code=404, detail=f"The post of id {id} is not found")
+    
+    #my_posts.pop(index) 
+    return Response(status_code=status.HTTP_204_NO_CONTENT) # if you send a response of 204, you shouldn't be sending any message back
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-     index = find_index(id)
-     
-     if index == None:
-          raise HTTPException(status_code=404, detail=f"The post of id {id} is not found")
-     post_dict = post.model_dump()
-     post_dict['id'] = id
-     my_posts[index] = post_dict
-     return {'data': post_dict }
+    #index = find_index(id)
+    
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if updated_post == None:
+        raise HTTPException(status_code=404, detail=f"The post of id {id} is not found")
+    
+    # post_dict = post.model_dump()
+    # post_dict['id'] = id
+    # my_posts[index] = post_dict
+    
+    return {'data': updated_post }
      
