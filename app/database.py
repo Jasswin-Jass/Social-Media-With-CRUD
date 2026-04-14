b9866601c7ff10ee25fa977b120060fdda1ab5c6
Jasswin-Jass <jasswinjass@gmail.com>
2026-04-14 22:38:53 +0530

    Updated

--- app/database.py
+++ app/database.py
@@ -7,3 +7,8 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:jass@@00@localhost/fastapi"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+""" All the above code is for setting up the database connection and create a session to interact with the database and also to create a base class for our models to inherit from.
+we can just copy and paste this code for other projects and just change the database and stuff and it will work fine. """
