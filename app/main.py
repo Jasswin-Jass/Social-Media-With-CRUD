7e7de865b9dd4459805e16396711dc67a1e10913
Jasswin-Jass <jasswinjass@gmail.com>
2026-05-12 00:14:21 +0530

    created route for creating a user

--- app/main.py
+++ app/main.py
@@ -127,3 +127,10 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 
     return post_query.first() 
 
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.model_dump()) # this will create a new post object based on the data that we have in the post object that we get from the request body and it will also convert the post object to a dictionary and then we can use the values of that dictionary to create a new post object and we can also use the model_dump() method to get the data from the post object in a dictionary format and then we use ** to unpack the dict and pass as eg. id=post.id 
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) # this will refresh the new_post object with the data from the database and it will also get the id of the new post that was created in the database and it is important to do this after committing the changes to the database because before committing, the new_post object will not have an id and it will be None and after committing, it will have an id and we can use that id to return the new post in the response.
+    return new_user
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, EmailStr
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -15,3 +15,13 @@ class PostResponse(PostBase):
     
     model_config = ConfigDict(from_attributes=True) # just use this line so pydantic can read the data from the database and convert it to a pydantic model and this is important because when we get the data from the database it will be in the form of an object and we need to convert it to a pydantic model so that we can return it in the response and also we can use this line to convert the data from the database to a pydantic model and then we can use that model to return the data in the response.
     #we can also skip this line cuz modern pydantic 2 can read this without the above line but it is a better practice to use it and ot avoid future errors 
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    model_config = ConfigDict(from_attributes=True)
